c89a9dce4437dedf25225dc2e5e9f69cf7eea67d
Greg Webb <gwebb@outlook.com>
2021-08-09 00:54:42 -0400

    updating css

--- src/components/VibeCard/VibeCard.jsx
+++ src/components/VibeCard/VibeCard.jsx
@@ -9,10 +9,8 @@ function VibeCard({ match}) {
     <Card href={`/${match[0]}`}>
       <Card.Content>
         <Card.Header>
-          <div className="match-item">
             <div className="emoji-small">{emojis[match[1]]}</div>
             <div className="name">{match[0]}</div>
-          </div>
         </Card.Header>
       </Card.Content>
       <Card.Content extra>
